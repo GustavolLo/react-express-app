56b3f0a4cfc56dfaf2ac898c8d502fe845162642
GustavolLo <goodluke111931@gmail.com>
2020-11-12 13:32:16 +0100

    Adding TaskList component.

--- src/server/defaultState.js
+++ src/server/defaultState.js
@@ -7,11 +7,30 @@ export const defaultState = {
         name: "To Do",
         id: "G1",
         owner: "U1"
+    },
+    {
+        name: "To Finish",
+        id: "G2",
+        owner: "U1"
     }],
     tasks: [{
         name: "Do tests",
         id: "T1",
         group: "G1",
+        owner: "U1",
+        isComplete: false
+    },
+    {
+        name: "Finish exam",
+        id: "T2",
+        group: "G2",
+        owner: "U1",
+        isComplete: false
+    },
+    {
+        name: "Finish cart",
+        id: "T2",
+        group: "G2",
         owner: "U1",
         isComplete: false
     }],
