fe242e4afec9fcda2ed40682a2bd65114cb07fff
GustavolLo <goodluke111931@gmail.com>
2020-11-12 17:36:25 +0100

    Adding Navigation, single link.

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../store';
 import { ConnectedDashboard } from './Dashboard';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { ConnectedNavigation } from './Navigation';
 
-
-export const  Main = () => (
-    <Provider store={store}>
-        <div>
-            <ConnectedDashboard />
-        </div>
-    </Provider>
+export const Main = () => (
+    <BrowserRouter>
+        <Provider store={store}>
+            <div>
+                <ConnectedNavigation />
+                <Route 
+                    exact
+                    path="/dashboard" 
+                    render={() => (<ConnectedDashboard />)}
+                />
+            </div>
+        </Provider>
+    </BrowserRouter>
 );
